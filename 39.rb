74168c44f811cad60f099653316a4ecef85bf9b8
John Figueiredo <jfigueir33@gmail.com>
2013-03-03 21:45:34 -0800

    39 and 40

--- 39.rb
+++ 39.rb
@@ -1,15 +1,13 @@
-def twenty_one?(*array)
-	sum = 0
-  array.each do |x|
-  	sum += x
-  end
-  if sum == 21
-  	puts true
-  else
-  	puts false
-  end
+# Exercise: Debugging ArgumentError - Part 2
+# Instead of changing the method invocation, change the definition.
+
+# In other words, rewrite the method so that it can take any number of arguments.
+
+def mean(*numbers)
+  sum = numbers.inject(:+)
+  
+  return sum / numbers.length
 end
 
-twenty_one?(3, 4, 5, 6, 3)
-twenty_one?(3, 11, 10)
-twenty_one?(10, 11)
+# This will throw an error. Change this line so that it works.
+sample_avg = mean(5, 3, 6, 10)
--- 40.rb
+++ 40.rb
@@ -1,24 +1,13 @@
-def sum(x)
-	sum = 0
-	while 0 < x
-		sum += x
-		x -= 1
-	end
-	sum
+# Exercise: Debugging ArgumentError - Part 1
+# Run the code. Read the error message.
+
+# Change the assigned value of sample_avg so that it does not throw an error.
+
+def mean(*numbers)
+  sum = numbers.inject(:+)
+  
+  return sum / numbers.length
 end
 
-puts sum(100)
-new_sum = sum(100) - sum(10)
-puts new_sum
-
-def sum_range(z)
-		sum = 0
-		y = 0
-		x = 1
-		while y < z
-			sum += y
-			y += 1
-		end
-end
-
-puts sum_range(556)
+# This will throw an error. Change this line so that it works.
+sample_avg = mean(5, 3, 6, 10)
